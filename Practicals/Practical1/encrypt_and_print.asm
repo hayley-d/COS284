33cf24c3803d02c75f05685b3c84f431f42b4ccc
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-08-13 15:33:56 +0200

    Implemented encrypt_and_print.asm

--- Practicals/Practical1/encrypt_and_print.asm
+++ Practicals/Practical1/encrypt_and_print.asm
@@ -27,68 +27,80 @@ print_char_32:
 
 encrypt_and_print:
     ; ask the user for an input string
-
+    call get_plaintext
+    call get_plaintext_input
+    call encrypt_plaintext
     ret
 
 get_plaintext:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, user_req
-    mov edx, 25
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, user_req
+    mov rdx, 25
     int 0x80
     ret
 
 get_plaintext_input:
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, input_string
-    mov edx, 100
+    mov rax, 3
+    mov rbx, 0
+    mov rcx, input_string
+    mov rdx, 100
     int 0x80
-    mov esi, input_string	;move the string into the esi
+    mov rsi, input_string	;move the string into the rsi
     ;get the end of the string
+    call find_null_term
 
 find_null_term:
-    mov al, [esi]
+    mov al, [rsi]
     cmp al, 0		;check if end of string
     je append_newline	;if found append newline
-    inc esi		;increment ptr
+    inc rsi		;increment ptr
     jmp find_null_term	;repeate until found \0
 
 append_newline:
-    mov byte[esi], 0x0A	;append newline char
-    mov byte[esi + 1], 0
+    mov byte[rsi], 0x0A	;append newline char
+    mov byte[rsi + 1], 0
     ret
+
 encrypt_plaintext:
-    mov esi, input_string
-    mov edi, encryption_string
-    mov eax, [hex_num]
+    mov rsi, input_string
+    mov rdi, encryption_string
+    mov rax, [hex_num]
+    jmp encryption_loop
 
 encryption_loop:    
-    mov al, [esi]
+    mov al, [rsi]
     test al, al
     jz print_result	;if null terminate jump to the print func
 
+    cmp al, 0x0A	;compare to \n
+    je skip_newline
+
     rol al, 4
-    xor al, byte [eax]
-    mov byte [edi], al	;store in the encrypted_string
+    xor al, byte [rax]
+    mov byte [rdi], al	;store in the encrypted_string
 
     ;go to the next character
-    inc esi
-    inc edi
+    inc rsi
+    inc rdi
+    jmp encryption_loop
+
+skip_newline:
+    inc rsi
     jmp encryption_loop
 
 print_result:
-    mov byte [edi], 0 ;add null terminator
-    mov eax, 4	      ;sys write call
-    mov ebx, 1	      ; 1 == stdout
-    mov ecx, result_message
-    mov edx, 21
+    mov byte [rdi], 0 ;add null terminator
+    mov rax, 4	      ;sys write call
+    mov rbx, 1	      ; 1 == stdout
+    mov rcx, result_message
+    mov rdx, 21
     int 0x80
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, encryption_string
-    mov edx, 100
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, encryption_string
+    mov rdx, 100
     int 0x80
 
     ret

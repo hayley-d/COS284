aa7c12fde9b77150402708057518d8515cbccd41
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-09 16:40:51 +0200

    basically done

--- Practicals/Practical4/task1_student_struct.asm
+++ Practicals/Practical4/task1_student_struct.asm
@@ -27,21 +27,16 @@ sub rsp, 64
 mov [rsp + 0], rdi
 mov [rsp + 4], rsi
 movss [rsp + 12], xmm0
-;get params into variables
-;mov dword[in_id], edi    ;move param 1 into id
-;mov [in_name], rsi ;move param 2 into name variable
-;movss dword[in_gpa], xmm0 ;move param 3 into variable
 xor rdi, rdi
 mov rdi, Student_size 
 call malloc
 mov [s], rax    ;save the pointer
-mov ebx, dword[in_id]
+mov ebx, dword[rsp + 0]
 mov [rax + Student.id], ebx ;move value of in_id into id]
 lea rdi, qword[rax + Student.name]
 mov rsi, qword[rsp + 4]
 call strcpy
 mov rax, [s]
-
 movss xmm0, [rsp + 12]
 movss [rax + Student.gpa], xmm0
 mov rax, [s]

e2e061be408186c3645b87b9046ac2a0482229ae
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-22 01:39:04 +0200

    finished implementing (untested)

---
+++ Practicals/Practical5/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h> 
+struct Pixel {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+    unsigned char cdfValue;
+    struct Pixel* up;
+    struct Pixel* down;
+    struct Pixel* left;
+    struct Pixel* right;
+};
+// Declaration of the external assembly function
+extern struct Pixel* createPixel(unsigned char red, unsigned char green, unsigned char blue ,struct Pixel* up, struct Pixel* down, struct Pixel* left, struct Pixel* right);
+
+int main() {
+    printf("Offset of red: %zu\n", offsetof(struct Pixel, red));
+    printf("Offset of green: %zu\n", offsetof(struct Pixel, green));
+    printf("Offset of blue: %zu\n", offsetof(struct Pixel, blue));
+    printf("Offset of cdfValue: %zu\n", offsetof(struct Pixel, cdfValue));
+    printf("Offset of up: %zu\n", offsetof(struct Pixel, up));
+    printf("Offset of down: %zu\n", offsetof(struct Pixel, down));
+    printf("Offset of left: %zu\n", offsetof(struct Pixel, left));
+    printf("Offset of right: %zu\n", offsetof(struct Pixel, right));
+
+    struct Pixel* p = createPixel(255,128,64,NULL,NULL,NULL,NULL);
+
+    // Access and print the pixel data from the allocated structure
+    printf("Pixel ID: %c\n", p->red); 
+    printf("Student Name: %c\n", p->green);
+    printf("Student GPA: %c\n", p->blue);
+    printf("CDF Value: %u\n", p->cdfValue);
+    free(p);
+
+  return 0;
+}
---
+++ Practicals/Practical5/task1_pixel_struct.asm
@@ -0,0 +1,67 @@
+
+section .data
+
+struc Pixel
+    .red:  resb 1       ; Reserve 1 byte for Red, Green and Blue 
+    .green: resb 1  
+    .blue: resb 1
+    .cdfValue: resb 1   ; Reserve 1 byte for char
+    align 8
+    .up: resq 1         ; Reserve 1 qword for ptr
+    .down: resq 1       ; Reserve 1 qword for ptr
+    .left: resq 1       ; Reserve 1 qword for ptr
+    .right: resq 1      ; Reserve 1 qword for ptr
+    align 8 
+endstruc
+
+p   dq  0               ; Temp variable to hold the new allocated memory
+
+extern strcpy
+extern malloc
+section .text
+    global  createPixel 
+
+createPixel:
+; Set up stack frame
+push rbp                
+mov rbp, rsp
+sub rsp, 64             ; Reserve 64 bytes
+
+; Get values for struct
+mov [rsp + 0], rdi      ; Store rdi = red
+mov [rsp + 1], rsi      ; Store rsi = green
+mov [rsp + 2], rdx      ; Store rdx = blue
+mov [rsp + 3], rcx      ; Store rcx = up ptr 
+mov [rsp + 11], r8      ; Store r8 = down ptr
+mov [rsp + 19], r9      ; Store r9 = left
+mov [rsp + 27], r11     ; Store r11 = right
+
+; Allocate memory for the struct
+xor rdi, rdi
+mov rdi, Pixel_size 
+call malloc
+mov [p], rax    ;save the pointer
+
+; Store values in the struct
+mov bl, byte[rsp + 0]                  ; Retrieve value for red
+mov [rax + Pixel.red], bl              ; Store the value for red
+mov bl, byte[rsp + 1]                  ; Retrieve value for green
+mov [rax + Pixel.red], bl              ; Store the value for green 
+mov bl, byte[rsp + 2]                  ; Retrieve value for blue
+mov [rax + Pixel.blue], bl             ; Store the value for blue
+
+mov bl, 0
+mov [rax + Pixel.cdfValue], bl         ; Inital value for cdf value os 0
+
+mov rbx, qword[rsp + 3]                 ; Retrieve ptr for up 
+mov [rax + Pixel.up], rbx               ; Store the address for up 
+mov rbx, qword[rsp + 11]                ; Retrieve ptr for down 
+mov [rax + Pixel.down], rbx             ; Store the address for down 
+mov rbx, qword[rsp + 19]                ; Retrieve ptr for left
+mov [rax + Pixel.left], ebx             ; Store the address for left
+mov rbx, qword[rsp + 27]                ; Retrieve ptr for right 
+mov [rax + Pixel.right], ebx            ; Store the address for right 
+mov rax, [p]                            ; Move ptr to the struct into rax
+leave
+ret
+
